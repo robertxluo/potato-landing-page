7b1da4e6d848906b005c29ef812c186df9e4ec01
robertxluo <robertxluo@gmail.com>
2019-08-27 20:46:43 -0500

    Make hamburger menu toggle to show links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-const toggleHamburgerMenu = x => x.classList.toggle('change');
+let nav = document.getElementById('navMobileLinks');
+nav.style.display = 'none';
+
+const toggleHamburgerMenu = x => {
+  x.classList.toggle('change');
+  if (nav.style.display === 'none') {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+  }
+};
